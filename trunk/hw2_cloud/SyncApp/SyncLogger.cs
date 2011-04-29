9e5c0a5bd0ee28df32f632ab98893767a0e02bd9
thebigkahoona <thebigkahoona@755ac375-fbb2-d23f-264c-d9a08988a8e8>
2011-04-29 11:51:45 +0000

    Working version

---
+++ trunk/hw2_cloud/SyncApp/SyncLogger.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.WindowsAzure.StorageClient;
+using Microsoft.WindowsAzure;
+
+namespace SyncApp
+{
+    public class SyncLoggerEntry : Microsoft.WindowsAzure.StorageClient.TableServiceEntity
+    {
+        public string ComputerName { get; set; }
+        public string FileName { get; set; }
+        public string Operation { get; set; }
+
+        public SyncLoggerEntry()
+        {
+            PartitionKey = "a";
+            RowKey = string.Format("{0:10}_{1}", DateTime.MaxValue.Ticks - DateTime.Now.Ticks, Guid.NewGuid());
+        }
+
+        override public string ToString()
+        {
+            return "Operation: " + Operation.ToString() + ", Computer Name: " + ComputerName + ", FileName: " + FileName;
+        }
+    };
+
+    public enum OperationType
+    {
+        FILE_CREATE, FILE_DELETE
+    };
+}
