0f7bb1ce2ec758d1b6d7e0d4da65aa5829cbd605
thebigkahoona <thebigkahoona@755ac375-fbb2-d23f-264c-d9a08988a8e8>
2011-06-07 13:32:43 +0000

    Statistics page done

--- hw2_cloud/SyncLibrary/CaptureEntry.cs
+++ hw2_cloud/SyncLibrary/CaptureEntry.cs
@@ -12,10 +12,11 @@ namespace SyncLibrary
     {
         public string id { get; set; } //The id of the capture request
         public string url { get; set; } //File URI on the storage            
-        public DateTime StartTime { get; set; } //Last modification date
-        public DateTime EndTime { get; set; } //Last modification date
-        public string WorkerId { get; set; } //More File info created by OS
+        public DateTime StartTime { get; set; } //Start proc time by the last worker who picked this task
+        public DateTime EndTime { get; set; } //End proc time
+        public string WorkerId { get; set; } //Responsible worker id
         public string blobUri { get; set; } //More File info created by OS
+        public Boolean done { get; set; }  //done proc?
         
         public CaptureEntry()
         {
